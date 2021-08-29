8b203ddafcef3b9533b1547657e8bef69299ad78
Andrew Choo <andydchoo@gmail.com>
2021-08-29 19:10:38 -0400

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,71 @@
+/*const times = ["12:00AM","1:00AM", "2:00AM", "3:00AM", "4:00AM", "5:00AM",
+       "6:00AM", "7:00AM", "8:00AM", "9:00AM", "10:00AM", "11:00AM",
+       "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM",
+       "6:00PM", "7:00PM", "8:00PM", "9:00PM", "10:00PM", "11:00PM",];
+*/
+
+//Array of time slots
+const times = [
+    {
+        id: "9AM",
+        time: "9",
+        ofDay: "AM",
+        event: ""
+    },
+
+    {
+        id: "10AM",
+        time: "10",
+        ofDay: "AM",
+        event: ""
+    },
+
+    {
+        id: "11AM",
+        time: "11",
+        ofDay: "AM",
+        event: ""
+    },
+
+    {
+        id: "12PM",
+        time: "12",
+        ofDay: "PM",
+        event: ""
+    },
+
+    {
+        id: "1PM",
+        time: "1",
+        ofDay: "PM",
+        event: ""
+    },
+
+    {
+        id: "3PM",
+        time: "3",
+        ofDay: "PM",
+        event: ""
+    },
+
+    {
+        id: "4PM",
+        time: "4",
+        ofDay: "PM",
+        event: ""
+    },
+
+    {
+        id: "5PM",
+        time: "5",
+        ofDay: "PM",
+        event: ""
+    }
+]
+
+function fillSlots() {
+    for (let i = 0; i < times.length; i++) {
+
+    }
+
+};
