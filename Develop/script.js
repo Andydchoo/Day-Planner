32926a41803a41c5a222bcaf9bfcecfedbfef355
Andrew Choo <andydchoo@gmail.com>
2021-10-03 21:24:21 -0400

    finished product

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,8 @@
-/*const times = ["12:00AM","1:00AM", "2:00AM", "3:00AM", "4:00AM", "5:00AM",
-       "6:00AM", "7:00AM", "8:00AM", "9:00AM", "10:00AM", "11:00AM",
-       "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM",
-       "6:00PM", "7:00PM", "8:00PM", "9:00PM", "10:00PM", "11:00PM",];
-*/
-
-//Array of time slots
+//An array for the time slot's data
 const times = [
     {
-        id: "9AM",
-        time: "9",
+        id: "09AM",
+        time: "09",
         ofDay: "AM",
         event: ""
     },
@@ -35,37 +29,84 @@ const times = [
     },
 
     {
-        id: "1PM",
-        time: "1",
+        id: "01PM",
+        time: "13",
         ofDay: "PM",
         event: ""
     },
 
     {
-        id: "3PM",
-        time: "3",
+        id: "02PM",
+        time: "14",
         ofDay: "PM",
         event: ""
     },
 
     {
-        id: "4PM",
-        time: "4",
+        id: "03PM",
+        time: "15",
         ofDay: "PM",
         event: ""
     },
 
     {
-        id: "5PM",
-        time: "5",
+        id: "04PM",
+        time: "16",
+        ofDay: "PM",
+        event: ""
+    },
+
+    {
+        id: "05PM",
+        time: "17",
         ofDay: "PM",
         event: ""
     }
 ]
 
+//A function for generating the different time slots
 function fillSlots() {
+
+    var currentTime = moment().hour();
+
     for (let i = 0; i < times.length; i++) {
+        var container = document.querySelector('.container');
+        var id = times[i].id;
+        var time = times[i].time;
+        var tenseClass = "";
 
+        if (time < currentTime) {
+            tenseClass = "past";
+
+        } else if (time == currentTime) {
+            tenseClass = "present";
+            
+        } else {
+            tenseClass = "future";
+        }
+
+        $('.container').append(`
+        <div class="${id} input-group mb-3 timeSlot">
+        <span class="input-group-text">${id}</span>
+        <input type="text" id="input${id}" class="form-control ${tenseClass}" value="${localStorage.getItem(`input${id}`) || ""}"></input>
+        <button class="btn btn-outline-secondary saveBtn" type="button" id="${id}btn">Add</button>
+        </div>`);
     }
-
 };
+
+//function for displaying current time at top of site
+$(document).ready(function () {
+
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+})
+
+fillSlots();
+
+//Function listening for savebtn click
+$(".saveBtn").click(function(event) {
+    var currentInput = $(this).parent('.timeSlot').find('input')[0];
+    var id = currentInput.id;
+
+    window.localStorage.setItem(id, currentInput.value);
+
+  });
